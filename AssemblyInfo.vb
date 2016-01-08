24762268ac103a985fc597a1f26950e03bce36a6
Ernst Peter Tamminga <ernst.peter.tamminga@xcess.nl>
2016-01-08 11:16:24 +0100

    Build update

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -49,7 +49,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("06.05.01.04")>
+<Assembly: AssemblyVersion("06.05.01.05")>
 
 <Assembly: AssemblyFileVersion("06.05.01.00")>
 <Assembly: ComVisibleAttribute(False)>
