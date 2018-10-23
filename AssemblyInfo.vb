324708ffe2fc0a016106176c601bb07ec0fd05d8
valadas <info@danielvaladas.com>
2018-10-23 18:16:38 -0400

    Fixed an issue with reCaptcha callback

--- AssemblyInfo.vb
+++ AssemblyInfo.vb
@@ -49,7 +49,7 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("06.06.01.08")>
+<Assembly: AssemblyVersion("06.06.01.11")>
 
 <Assembly: AssemblyFileVersion("06.06.01.00")>
 <Assembly: ComVisibleAttribute(False)>
